721b7e09c5bf95d600587931265c9453d463871d
Jay Katyan <33378744+Jkatyan@users.noreply.github.com>
2022-03-26 17:22:03 -0400

    :white_check_mark: Google Authentication

--- web-app/project/src/App.js
+++ web-app/project/src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {GoogleLogin} from 'react-google-login'
+
+const responseGoogle = (response) => {
+  console.log(response);
+} 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1> Calendar API Login</h1>
+        </div>
+        <div>
+          <GoogleLogin clientId='1062515714621-35tek1at77jjse1o47cqa1trok77uses.apps.googleusercontent.com'
+                       buttonText='Sign In'
+                       onSuccess={responseGoogle}
+                       onFailure={responseGoogle}
+                       cookiePolicy='single_host_origin'
+                       responseType='code'
+                       accessType='code'
+                       scope='openid email profile https://www.googleapis.com/auth/calendar'
+                       />
+        </div>
       </header>
     </div>
   );
