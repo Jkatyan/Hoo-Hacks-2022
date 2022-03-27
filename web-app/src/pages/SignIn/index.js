8843d089c4bb8795fffa2a24ef1ecced03260d80
Jay Katyan <33378744+Jkatyan@users.noreply.github.com>
2022-03-27 03:41:24 -0400

    :hammer: Backend integrated with web-app

--- web-app/src/pages/SignIn/index.js
+++ web-app/src/pages/SignIn/index.js
@@ -1,79 +1,60 @@
-import React, { Component } from 'react';
-import {Link, withRouter} from 'react-router-dom';
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
 
-
+import axios from "axios";
+import { GoogleLogin } from "react-google-login";
 
 class SignIn extends Component {
-    componentWillMount(){
-        document.getElementById('body').className='bg-gradient-primary'
-      }
+  componentWillMount() {
+    document.getElementById("body").className = "bg-gradient-primary";
+  }
 
-      handleSignIn = () => {
+  handleSignIn = (response) => {
+    console.log(response);
+    const { code } = response;
+    axios
+      .post("/api/create-tokens", { code })
+      .then((response) => {
+        console.log(response.data);
         this.props.history.push("/dashboard");
-    }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
-    render() {
-        return (
-            <div className="container">
-            {/* <!-- Outer Row --> */}
-            <div className="row justify-content-center">
-        
-              <div className="col-xl-10 col-lg-12 col-md-9">
-        
-                <div className="card o-hidden border-0 shadow-lg my-5">
-                  <div className="card-body p-0">
-                    {/* <!-- Nested Row within Card Body --> */}
-                    <div className="row">
-                      <div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                      <div className="col-lg-6">
-                        <div className="p-5">
-                          <div className="text-center">
-                            <h1 className="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                          </div>
-                          <form onSubmit={this.handleSignIn} className="user">
-                            <div className="form-group">
-                              <input type="email" className="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..."/>
-                            </div>
-                            <div className="form-group">
-                              <input type="password" className="form-control form-control-user" id="exampleInputPassword" placeholder="Password"/>
-                            </div>
-                            <div className="form-group">
-                              <div className="custom-control custom-checkbox small">
-                                <input type="checkbox" className="custom-control-input" id="customCheck"/>
-                                <label className="custom-control-label" for="customCheck">Remember Me</label>
-                              </div>
-                            </div>
-                            <button  type="submit" className="btn btn-primary btn-user btn-block">
-                              Login
-                            </button>
-                            <hr/>
-                            <a href="index.html" className="btn btn-google btn-user btn-block">
-                              <i className="fab fa-google fa-fw"></i> Login with Google
-                            </a>
-                            <a href="index.html" className="btn btn-facebook btn-user btn-block">
-                              <i className="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                            </a>
-                          </form>
-                          <hr/>
-                          <div className="text-center">
-                            <a className="small" href="forgot-password.html">Forgot Password?</a>
-                          </div>
-                          <div className="text-center">
-                            <Link className="small" to="/signup">Create an Account!</Link>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-        
+  render() {
+    return (
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="App-header">
+            <h1>
+              Welcome to <b>OnTrack</b>
+            </h1>
+            <h4>
+              An <code>ML-Based</code> Homework Tracker
+            </h4>
+            <hr />
+            <div>
+              <div className="user">
+                <GoogleLogin
+                  className="btn btn-google btn-user btn-block"
+                  clientId="1062515714621-35tek1at77jjse1o47cqa1trok77uses.apps.googleusercontent.com"
+                  buttonText="Sign In"
+                  onSuccess={this.handleSignIn}
+                  onFailure={this.handleSignIn}
+                  cookiePolicy="single_host_origin"
+                  responseType="code"
+                  accessType="code"
+                  scope="openid email profile https://www.googleapis.com/auth/calendar"
+                />
               </div>
-        
             </div>
-        
           </div>
-        )
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withRouter(SignIn);
